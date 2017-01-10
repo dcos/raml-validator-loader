b79723fbdbedb8b93af6e68f6ff67ae64f14cc7c
Ioannis Charalampidis <johnys2@gmail.com>
2017-01-10 11:42:30 +0100

    Making webpack module accept query arguments

--- src/raml-validator-loader.js
+++ src/raml-validator-loader.js
@@ -20,25 +20,58 @@ module.exports = function (source) {
   // 1) Provide a custom content for the source file
   // 2) Track dependant files in order for webpack to invalidate caches
   //
-  var raml = RAML.loadApiSync(this.resourcePath, {
+  const raml = RAML.loadApiSync(this.resourcePath, {
     fsResolver: {
-      content: (function (path) {
+      content: (path) => {
         if (path === this.resourcePath) {
           return source;
         }
 
+        // When RAML loader is requesting a file, trackit as a dependency
+        // so webpack can do it's magic internally.
         this.addDependency(path);
 
         return fs.readFileSync(path).toString();
-      }).bind(this)
+      }
     }
   });
 
+  // Parse URL parameters to generator configuration
+  let config = {};
+  if (this.query) {
+    const query = this.query.substr(1);
+    if (query[0] === '{') {
+      config = JSON.parse(query);
+
+    } else {
+      config = query.split('&').reduce((memo, kv) => {
+        let [key, value] = kv.split('=');
+        if (!key) {
+          return memo;
+        }
+
+        // Do some type conversion
+        if (value === 'true') {
+          value = true;
+        } else if (value === 'false') {
+          value = false;
+        } else if (!isNaN(value)) {
+          value = Number(value);
+        }
+
+        // Keep the config
+        memo[key] = value;
+
+        return memo;
+      }, {});
+    }
+  }
+
   // Prepare generator context
-  var ctx = new GeneratorContext();
+  const ctx = new GeneratorContext(config);
 
   // Use all types in this RAML specification
-  raml.types().forEach(function (type) {
+  raml.types().forEach((type) => {
     ctx.uses( type.runtimeType() );
   });
 
